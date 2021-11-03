8ed54485ec743bf8387c2d0abf4ecf8a59bfe2d8
khsabrina <sabrina.khazanov@gmail.com>
2021-11-03 12:18:39 -0700

    five

--- main.c
+++ main.c
@@ -16,29 +16,29 @@ int main(){
     int j;
     for (i=1;i<=4;i++){
         if(i==1){
-            printf("The Armstrong numbers are: ");
+            printf("The Armstrong numbers are:");
         }
         if(i==2){
-            printf("The Palindromes are: ");
+            printf("The Palindromes are:");
         }
         if(i==3){
-            printf("The Prime numbers are: ");
+            printf("The Prime numbers are:");
         }
         if(i==4){
-            printf("The Strong numbers are: ");
+            printf("The Strong numbers are:");
         }
         for(j=num2;j<=num1;j++){
             if(i==1 && isArmstrong(j)==1){
-                printf("%d ",j);
+                printf(" %d",j);
             }
             if(i==2 && isPalindrome(j)==1){
-                printf("%d ",j);
+                printf(" %d",j);
             }
             if(i==3 && isPrime(j)==1){
-                printf("%d ",j);
+                printf(" %d",j);
             }
             if(i==4 && isStrong(j)==1){
-                printf("%d ",j);
+                printf(" %d",j);
             }
         
         }
